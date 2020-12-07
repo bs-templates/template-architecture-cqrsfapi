c59991e5e423766bdb9b4b0ab1dcd17a7ffb8c4b
Ítalo Silveira <italo-silveira@impulso.network>
2020-12-06 23:49:58 -0300

    Updates
    - update to .net 5.0
    - add migrations

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # template-architecture-cqrsfapi
 Architecture CQRS FullAPI 
+
+cd src/BAYSOFT.Presentations.WebAPI
+
+dotnet ef migrations add InitialDefaultDb -c DefaultDbContext -o Migrations
---
+++ src/BAYSOFT.Presentations.WebAPI/Migrations/20201207024221_InitialDefaultDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BAYSOFT.Presentations.WebAPI.Migrations
+{
+    public partial class InitialDefaultDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Samples",
+                columns: table => new
+                {
+                    SampleID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samples", x => x.SampleID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Samples");
+        }
+    }
+}
